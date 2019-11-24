46f85cf25bd60c6f4089f6253bce8e851ba7ba0f
serena-ramley <sramley@gmail.com>
2019-11-23 22:56:32 -0800

    Create day17JudgeVegetables.js

---
+++ day17JudgeVegetables.js
@@ -0,0 +1,5 @@
+
+const judgeVegetable = (vegetables, metric) => {
+  return   vegetables.reduce( (winner, contestant) =>
+                          contestant[metric] > winner[metric] ? contestant : winner).submitter
+}
